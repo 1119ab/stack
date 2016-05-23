1e1ad9d6c7835d33c3cf973d3950316881be2444
1119ab <t.syunsuke111965@gmail.com>
2016-05-23 15:02:52 +0900

    second commit

---
+++ push.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+    int p, n[3];
+
+	/*char stack[100];
+	
+
+    //scanf("%d\n"&n[p]);
+
+
+    stack[p] = atoi;
+	void push(int a){
+		stack[p]=a;
+		p++;
+	}
+	
+int pop(){int a;
+	a=stack[p-1];
+	p--;
+    
+return a;
+
+}*/
+for(p=0;p < 3;p++){scanf("%d",&n[p]);};
+
+for(p=0;p < 3;p++){
+//if(p >= 0){
+	printf("%d\n",n[p]);
+      //    p--;
+  //    };
+}
+
+
+}
