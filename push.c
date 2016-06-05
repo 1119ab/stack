cc85944bca74048ae1add115667c63ae511181c2
1119ab <t.syunsuke111965@gmail.com>
2016-06-05 18:57:58 +0900

    stack.c

--- push.c
+++ push.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main(){
-
-    int p,q,m;
+    int p,q,a,b,c;
 	int stack[100];
 	void push(int a){
 		stack[p]=a;
 		p++;
+
 	}
 	
 int pop(){int a;
@@ -14,19 +13,58 @@ int pop(){int a;
 	p--;    
 return a;
 }
-void add (int a, int b) {
-	int c;
+void add () {
 	a=pop();
 	b=pop();
 	c=a+b;
     push(c);
-    return 0;
+    
 }
+void minus (){
+	
+	a=pop();
+    b=pop();
+    c=b-a;
+    push(c);
+}
+void cross (){
+	
+	a=pop();
+	b=pop();
+	c=a*b;
+	push(c);
+}
+void waru (){
+	a=pop();
+	b=pop();
+	c=b/a;
+	push(c);
+}
+int main(){
+char m[10];
 for(q=0;q >= 0;q++){
-	scanf("%d",&m);
-	if(m == 0){printf("%d\n",pop()); return 0;}
-	else if(m == 10){add();}
-    else(push(m));}
+	scanf("%s",m);
 
-
+	/*if(*m == '='){printf("%d\n",pop()); return 0;}
+	else if(*m == '+'){add();}
+	else if(*m == '-'){minus();}
+	else if(*m == '*'){cross();}
+	else if(*m == '/'){waru();}
+    else(push(atoi(m)));*/
+    switch (*m) {
+    	case '+' : add();
+    	break;
+    	case '-' : minus();
+    	break;
+    	case '*' : cross();
+    	break;
+    	case '/' : waru();
+    	break;
+    	case '=' : printf("%d\n",pop());return 0;
+    	break;
+    	default : push(atoi(m));
+        break;    
+    }
 }
+    }
+
