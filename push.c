37dc8ea74f8c2554295bb884fe7519c06aaa021f
1119ab <t.syunsuke111965@gmail.com>
2016-06-17 14:30:12 +0900

    逆ポーランド記法

--- push.c
+++ push.c
@@ -1,56 +1,37 @@
 #include <stdio.h>
-
     int p,q,a,b,c;
 	int stack[100];
 	void push(int a){
 		stack[p]=a;
-		p++;
-
-	}
-	
+		p++;}	
 int pop(){int a;
 	a=stack[p-1];
 	p--;    
-return a;
-}
+	c=a+b;
+return a;}
 void add () {
 	a=pop();
 	b=pop();
-	c=a+b;
-    push(c);
-    
-}
-void minus (){
-	
+    push(c);}
+void minus (){	
 	a=pop();
     b=pop();
     c=b-a;
-    push(c);
-}
-void cross (){
-	
+    push(c);}
+void cross (){	
 	a=pop();
 	b=pop();
 	c=a*b;
-	push(c);
-}
+	push(c);}
 void waru (){
 	a=pop();
 	b=pop();
 	c=b/a;
-	push(c);
-}
+	push(c);}
 int main(){
 char m[10];
 for(q=0;q >= 0;q++){
 	scanf("%s",m);
-
-	/*if(*m == '='){printf("%d\n",pop()); return 0;}
-	else if(*m == '+'){add();}
-	else if(*m == '-'){minus();}
-	else if(*m == '*'){cross();}
-	else if(*m == '/'){waru();}
-    else(push(atoi(m)));*/
     switch (*m) {
     	case '+' : add();
     	break;
@@ -60,11 +41,9 @@ for(q=0;q >= 0;q++){
     	break;
     	case '/' : waru();
     	break;
-    	case '=' : printf("%d\n",pop());return 0;
+    	case '=' : printf("%d\n",pop());
+    	return 0;
     	break;
     	default : push(atoi(m));
-        break;    
-    }
-}
-    }
+        break;}}}
 
