023d98217870e0077677407873b60c8d226cb14e
1119ab <t.syunsuke111965@gmail.com>
2016-06-28 16:14:27 +0900

    push.c

--- push.c
+++ push.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
-    int p,q,a,b,c;
+    int p=0;
 	int stack[100];
-	void push(int a){
+
+void push(int a){
 		stack[p]=a;
 		p++;}	
+
 int pop(){int a;
 	a=stack[p-1];
 	p--;    
-	c=a+b;
-return a;}
+	return a;
+    }
+
 void add () {
+    push(pop()+pop());
+    }
+
+void minus (){int a,b;
 	a=pop();
 	b=pop();
-	c=b+a;
-    push(c);}
-void minus (){	
-	a=pop();
-    b=pop();
-    c=b-a;
-    push(c);}
+    push(b-a);
+    }
+
 void cross (){	
+	push(pop()*pop());
+    }
+
+void waru (){int a,b;
 	a=pop();
 	b=pop();
-	c=a*b;
-	push(c);}
-void waru (){
-	a=pop();
-	b=pop();
-	c=b/a;
-	push(c);}
+	push(b/a);
+    }
+
 int main(){
-char m[10];
-for(q=0;q >= 0;q++){
-	scanf("%s",m);
-    switch (*m) {
-    	case '+' : add();
-    	break;
-    	case '-' : minus();
-    	break;
-    	case '*' : cross();
-    	break;
-    	case '/' : waru();
-    	break;
-    	case '=' : printf("%d\n",pop());
-    	return 0;
-    	break;
-    	default : push(atoi(m));
-        break;}}}
+	char m[10];
+	while(1){ 
+		scanf("%s",m);
+	    switch (*m) {
+	    	case '+' : add();
+	    	break;
+	    	case '-' : minus();
+	    	break;
+	    	case '*' : cross();
+	    	break;
+	    	case '/' : waru();
+	    	break;
+	    	case '=' : printf("%d\n",pop());
+	    	return 0;
+	    	default : push(atoi(m));
+	        break;
+	    }
+	}
+}
 
